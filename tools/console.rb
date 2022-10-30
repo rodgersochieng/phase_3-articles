3403934488f0be95a31de4ce8ac61bbf161ed3fa
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-30 11:22:20 +0300

    consol

--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,19 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+author_1 = Author.new("F.BETT")
+author_2 = Author.new("P.PETERS")
 
+magazine_1 = Magazine.new("Peoples Daily", "News")
+magazine_2 = Magazine.new("The Standard", "Auctions")
+
+article_1 = Article.new(author_1, magazine_1, "Dairing Abroad")
+article_2 = Article.new(author_1, magazine_2, "Kenyans Abroad")
+article_3 = Article.new(author_2, magazine_1, "Should I")
+article_4 = Article.new(author_2, magazine_2, "Life from the other side")
+
+pp author_1.articles
+pp author_2.articles
 
 
 
